3edc1ae43ed34812dae318006e654c4d4b078d2e
Olena <elena.cerg@gmail.com>
2022-10-25 00:07:09 +0300

    fix in func popular_courses

--- Django/online_school/main/templatetags/costom_tags.py
+++ Django/online_school/main/templatetags/costom_tags.py
@@ -37,7 +37,7 @@ def popular_courses(num: int = 5) -> dict:
             courses.update({student.group.course_id: 1})
     sorted_courses = dict(sorted(courses.items(), key=lambda item: item[1], reverse=True)[:num])
     return {
-        'course_list': Course.objects.filter(id__in=sorted_courses.keys())
+        'course_list': Course.objects.prefetch_related('teacher').filter(id__in=sorted_courses.keys())
     }
 
 
