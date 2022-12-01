3e24df928eba9c565aec12bc829ef6372af9ae50
Olena <elena.cerg@gmail.com>
2022-12-01 12:23:07 +0200

    cache

--- Django/online_school/online_school/settings.py
+++ Django/online_school/online_school/settings.py
@@ -191,6 +191,19 @@ REST_FRAMEWORK = {
     ]
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+        'LOCATION': '127.0.0.1:11211',
+        'OPTIONS': {
+            'no_delay': True,
+            'ignore_exc': True,
+            'max_pool_size': 4,
+            'use_pooling': True,
+        }
+    }
+}
+
 try:
     from .local_settings import *
 except ImportError:
