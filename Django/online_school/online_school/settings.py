dd5b272892fa0b9ee5f5735533823b2a6a99e163
Olena <elena.cerg@gmail.com>
2022-12-03 22:19:16 +0200

    fix caches settings

--- Django/online_school/online_school/settings.py
+++ Django/online_school/online_school/settings.py
@@ -194,7 +194,7 @@ REST_FRAMEWORK = {
 CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
-        'LOCATION': '127.0.0.1:11211',
+        'LOCATION': 'memcached:11211',
         'OPTIONS': {
             'no_delay': True,
             'ignore_exc': True,
